8e171873a6d36ab5e8763660fa1f43e19ae3ed55
James Hall-Treworgy <jameshall-treworgy@Jamess-MacBook-Pro.local>
2022-08-15 10:23:08 -0400

    Moved function calls to account.

--- main.go
+++ main.go
@@ -13,144 +13,76 @@ import (
 	user "os/user"
 )
 
-var DirPath = ""
+var BasePath = ""
 var PathToAccounts = ""
 var PathToOutput = ""
+var AccControl Acc.AccountController
 
 func main() {
-	var args = os.Args
-	var user, _ = user.Current()
+	var args []string = os.Args[1:]
 
-	DirPath = user.HomeDir + "/.AcountManager"
-
+	setBasePath()
 	setOutputPath()
 	setAccountsPath()
 	createApplicationDir()
 	createAccountsFile()
 
-	if len(args) <= 1 {
+	AccControl = Acc.AccountController{
+		DataPath: PathToAccounts,
+	}
+
+	if len(args) < 1 {
 		fmt.Println("Try Commands: ")
-		printCommands()
+		listCommands()
 		return
 	}
 
-	var allArgsAfter []string = os.Args[1:]
-	var firstCmd = allArgsAfter[0]
+	var firstCmd = args[0]
 
 	if firstCmd == "list" {
-		listAccounts(allArgsAfter)
+		AccControl.ListAccounts(args)
 		return
 	}
 
-	if len(allArgsAfter) < 2 {
+	if len(args) < 2 {
 		fmt.Println("More Arguments required")
 		return
 	}
 
 	if firstCmd == "add" {
-		addAccount(allArgsAfter)
+		AccControl.AddAccount(args)
 		return
 	}
 
 	if firstCmd == "update" {
-		updateAccount(allArgsAfter)
+		AccControl.UpdateAccount(args)
 		return
 	}
 
 	if firstCmd == "delete" {
-		deleteAccount(allArgsAfter)
+		AccControl.DeleteAccount(args)
 		return
 	}
 
-	handleAccountRequest(allArgsAfter)
+	handleGetAccountInfoRequest(args)
 }
 
-func printCommands() {
+func listCommands() {
 	var output = `
 	list    - List All Accounts
 	add     - Add an Account
 	update  - Update Account by Account Type
+	delete  - Delete Account by Account Type
 	`
 
 	fmt.Println(output)
 }
 
-func listAccounts(args []string) {
-	var accounts = getAllAccounts()
-	var output string = ""
-
-	for _, account := range accounts {
-		output += "Type: " + account.Type + " | Username: " + account.Username + "\n"
-	}
-
-	fmt.Println(output)
-}
-
-func deleteAccount(args []string) {
-	var accType = args[1]
-	var allAccounts = getAllAccounts()
-	var newAccounts []Acc.Account
-
-	for _, account := range allAccounts {
-		if account.Type != accType {
-			newAccounts = append(newAccounts, account)
-		}
-	}
-
-	var res, _ = Json.Marshal(newAccounts)
-	os.WriteFile(PathToAccounts, res, 0644)
-}
-
-// Full Command
-// ./Main add microsoft {username} {password}
-func addAccount(args []string) {
-	var accType = args[1]
-	var username = args[2]
-	var password = args[3]
-
-	var allAccounts = getAllAccounts()
-	var newAccount = Acc.Account{
-		Type:     accType,
-		Username: username,
-		Password: password,
-	}
-
-	allAccounts = append(allAccounts, newAccount)
-
-	var res, _ = Json.Marshal(allAccounts)
-	os.WriteFile(PathToAccounts, res, 0644)
-}
-
-// Full Command
-// ./Main update microsoft {username} {password}
-func updateAccount(args []string) {
-	var accType = args[1]
-	var username = args[2]
-	var password = args[3]
-
-	var newAccount = Acc.Account{
-		Type:     accType,
-		Username: username,
-		Password: password,
-	}
-
-	var allAccounts = getAllAccounts()
-
-	for index, account := range allAccounts {
-		if account.Type == accType {
-			allAccounts[index] = newAccount
-		}
-	}
-
-	var res, _ = Json.Marshal(allAccounts)
-	os.WriteFile(PathToAccounts, res, 0644)
-}
-
-func handleAccountRequest(args []string) {
+func handleGetAccountInfoRequest(args []string) {
 	var accountType = args[0]
 	var key = args[1]
-	var allAccounts = getAllAccounts()
-	var account, accountErr = Acc.FindAccount(allAccounts, accountType)
+	var accounts = AccControl.GetAll()
+	var account, accountErr = AccControl.Find(accounts, accountType)
 
 	if accountErr != nil {
 		fmt.Println(accountErr)
@@ -169,16 +101,21 @@ func handleAccountRequest(args []string) {
 }
 
 func setOutputPath() {
-	PathToOutput = DirPath + "/output.txt"
+	PathToOutput = BasePath + "/output.txt"
+}
+
+func setBasePath() {
+	var user, _ = user.Current()
+	BasePath = user.HomeDir + "/.AcountManager"
 }
 
 func setAccountsPath() {
-	PathToAccounts = DirPath + "/accounts.json"
+	PathToAccounts = BasePath + "/accounts.json"
 }
 
 func createApplicationDir() {
-	if !File.PathExists(DirPath) {
-		File.CreatePath(DirPath)
+	if !File.PathExists(BasePath) {
+		File.CreatePath(BasePath)
 	}
 }
 
@@ -188,22 +125,4 @@ func createAccountsFile() {
 		var res, _ = Json.Marshal(value)
 		os.WriteFile(PathToAccounts, res, 0644)
 	}
-}
-
-func getAllAccounts() []Acc.Account {
-	var file = File.ReadFile(PathToAccounts)
-
-	if file != nil {
-		return parseJson(file)
-	}
-
-	return nil
-}
-
-func parseJson(b []byte) []Acc.Account {
-	var accounts []Acc.Account
-
-	Json.Unmarshal(b, &accounts)
-
-	return accounts
 }
--- packages/account/account.go
+++ packages/account/account.go
@@ -1,6 +1,12 @@
 package account
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	Json "encoding/json"
+	File "main/packages/myfile"
+)
 
 type Account struct {
 	Type     string
@@ -8,7 +14,11 @@ type Account struct {
 	Password string
 }
 
-func FindAccount(accounts []Account, accountType string) (Account, error) {
+type AccountController struct {
+	DataPath string
+}
+
+func (acc AccountController) Find(accounts []Account, accountType string) (Account, error) {
 	var output Account
 
 	for _, account := range accounts {
@@ -22,4 +32,94 @@ func FindAccount(accounts []Account, accountType string) (Account, error) {
 	}
 
 	return output, nil
+}
+
+func (acc AccountController) GetAll() []Account {
+	var file = File.ReadFile(acc.DataPath)
+
+	if file != nil {
+		return parseAccountsJson(file)
+	}
+
+	return nil
+}
+
+func (acc AccountController) UpdateAccounts(accounts []Account) {
+	var res, _ = Json.Marshal(accounts)
+	os.WriteFile(acc.DataPath, res, 0644)
+}
+
+// Full Command
+// ./Main update microsoft {username} {password}
+func (acc AccountController) UpdateAccount(args []string) {
+	var accType = args[1]
+	var username = args[2]
+	var password = args[3]
+
+	var newAccount = Account{
+		Type:     accType,
+		Username: username,
+		Password: password,
+	}
+
+	var allAccounts = acc.GetAll()
+
+	for index, account := range allAccounts {
+		if account.Type == accType {
+			allAccounts[index] = newAccount
+		}
+	}
+
+	acc.UpdateAccounts(allAccounts)
+}
+
+func (acc AccountController) ListAccounts(args []string) {
+	var accounts = acc.GetAll()
+	var output string = ""
+
+	for _, account := range accounts {
+		output += "Type: " + account.Type + " | Username: " + account.Username + "\n"
+	}
+
+	fmt.Println(output)
+}
+
+func (acc AccountController) DeleteAccount(args []string) {
+	var accType = args[1]
+	var accounts = acc.GetAll()
+	var newAccounts []Account
+
+	for _, account := range accounts {
+		if account.Type != accType {
+			newAccounts = append(newAccounts, account)
+		}
+	}
+
+	acc.UpdateAccounts(newAccounts)
+}
+
+// Full Command
+// ./Main add microsoft {username} {password}
+func (acc AccountController) AddAccount(args []string) {
+	var accType = args[1]
+	var username = args[2]
+	var password = args[3]
+
+	var accounts = acc.GetAll()
+	var newAccount = Account{
+		Type:     accType,
+		Username: username,
+		Password: password,
+	}
+
+	accounts = append(accounts, newAccount)
+	acc.UpdateAccounts(accounts)
+}
+
+func parseAccountsJson(b []byte) []Account {
+	var accounts []Account
+
+	Json.Unmarshal(b, &accounts)
+
+	return accounts
 }
